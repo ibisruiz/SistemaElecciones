aee0e1f202418fa6a16955349478af83d82515ed
javier1292 <66648525+javier1292@users.noreply.github.com>
2021-12-10 15:20:56 -0400

    view elector

--- controllers/ElectorController.js
+++ controllers/ElectorController.js
@@ -10,3 +10,36 @@ exports.getMenuElector = (req, res, next) => {
     res.status(200).render("elector/menuElector", { layout: false });
 
 };
+
+exports.getPuestoE = (req, res, next) => {
+
+    res.status(200).render("elector/puestos", { layout: false });
+
+};
+
+exports.getcandidatoR = (req, res, next) => {
+
+    res.status(200).render("elector/candidaroR", { layout: false });
+
+};
+
+
+exports.getcandidatoD = (req, res, next) => {
+
+    res.status(200).render("elector/candidatoD", { layout: false });
+
+};
+
+
+exports.getcandidatoP = (req, res, next) => {
+
+    res.status(200).render("elector/CandidatoP", { layout: false });
+
+};
+
+
+exports.getcandidatoS = (req, res, next) => {
+
+    res.status(200).render("elector/candidatoS", { layout: false });
+
+};
--- routes/elector.js
+++ routes/elector.js
@@ -5,5 +5,10 @@ const electorController = require("../controllers/ElectorController")
 
 router.get("/", electorController.GetIndex);
 router.get("/elector-menu", electorController.getMenuElector);
+router.get("/elector-puestos",electorController.getPuestoE);
+router.get("/elector-candidatoR",electorController.getcandidatoR);
+router.get("/elector-candidatoD",electorController.getcandidatoD);
+router.get("/elector-candidatoP",electorController.getcandidatoP);
+router.get("/elector-candidatoS",electorController.getcandidatoS);
 
 module.exports = router;
